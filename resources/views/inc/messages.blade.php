aff5859d24ca8355e31a814dd524a327f78ed2ff
shamsuddin2021 <shamsuddin2021@gmail.com>
2020-04-06 21:17:56 +0600

    Laravel web development project master

---
+++ resources/views/inc/messages.blade.php
@@ -0,0 +1,27 @@
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+
+
+@if(count($errors) > 0)
+	@foreach($errors->all() as $error)
+	<div class="alert alert-danger">
+		{{$error}}
+	</div>
+	@endforeach
+@endif
+
+@if(session('success'))
+	<div class="alert alert-success">
+		{{session('success')}}
+	</div>
+@endif
+
+@if(session('error'))
+	<div class="alert alert-danger">
+		{{session('error')}}
+	</div>
+@endif
+
+
+	</div>
+</div>
